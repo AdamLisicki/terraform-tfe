81e6718171e84a9a95b3e6415a948956db36ae7a
Adam Lisicki <adam.lisicki@adamlisicki.pl>
2023-10-22 20:42:13 +0200

    remove product-service workspace

--- locals_tfe.tf
+++ locals_tfe.tf
@@ -18,25 +18,25 @@ locals {
       project_id          = module.project["engineering-thesis-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/terraform-github"
     }
-  
-    "product-service-prod" = {
-      description         = "Automation for product service resources."
-      execution_mode      = "remote"
-      project_id          = module.project["engineering-thesis-project"].id
-      vcs_repo_identifier = "${var.github_organization_name}/terraform-product-service"
+  }
+    # "product-service-prod" = {
+    #   description         = "Automation for product service resources."
+    #   execution_mode      = "remote"
+    #   project_id          = module.project["engineering-thesis-project"].id
+    #   vcs_repo_identifier = "${var.github_organization_name}/terraform-product-service"
 
-      variables = [
-        {
-          category = "terraform"
-          key      = "cluster_name"
-          value    = "cluster-prod"
-        },
-        {
-          category = "terraform"
-          key      = "environment"
-          value    = "prod"
-        },
-      ]
-    }
-    }
+    #   variables = [
+    #     {
+    #       category = "terraform"
+    #       key      = "cluster_name"
+    #       value    = "cluster-prod"
+    #     },
+    #     {
+    #       category = "terraform"
+    #       key      = "environment"
+    #       value    = "prod"
+    #     },
+    #   ]
+    # }
+    # }
 }
